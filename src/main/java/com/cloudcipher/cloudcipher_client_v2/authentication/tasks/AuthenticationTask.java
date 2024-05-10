a43716e4f9be6fcc4efc81c375a16389fd6b9c6e
ana117 <63540951+ana117@users.noreply.github.com>
2024-05-10 17:18:08 +0700

    Refactor WebUtility to static

--- src/main/java/com/cloudcipher/cloudcipher_client_v2/authentication/tasks/AuthenticationTask.java
+++ src/main/java/com/cloudcipher/cloudcipher_client_v2/authentication/tasks/AuthenticationTask.java
@@ -3,14 +3,13 @@ package com.cloudcipher.cloudcipher_client_v2.authentication.tasks;
 import com.cloudcipher.cloudcipher_client_v2.authentication.model.AuthenticationResponse;
 import javafx.concurrent.Task;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
-import utility.WebUtility;
+import com.cloudcipher.cloudcipher_client_v2.utility.WebUtility;
 
 public class AuthenticationTask extends Task<AuthenticationResponse> {
 
     private final String username;
     private final String password;
     private final String type;
-    private final WebUtility webUtility = new WebUtility();
 
     public AuthenticationTask(String username, String password, String type) {
         this.username = username;
@@ -20,11 +19,11 @@ public class AuthenticationTask extends Task<AuthenticationResponse> {
 
     @Override
     protected AuthenticationResponse call() {
-        String url = webUtility.getServerUrl() + this.type;
+        String url = WebUtility.getServerUrl() + this.type;
         MultipartEntityBuilder builder = MultipartEntityBuilder.create()
                 .addTextBody("username", this.username)
                 .addTextBody("password", this.password);
 
-        return webUtility.authRequest(url, builder);
+        return WebUtility.authRequest(url, builder);
     }
 }
