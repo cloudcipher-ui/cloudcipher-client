72d6bff02e338b793a25019ea8320a5b338c7d9a
ana117 <63540951+ana117@users.noreply.github.com>
2024-05-30 14:06:37 +0700

    Revert "Add debug button to test local functionality"
    This reverts commit 6fc323b6b72d647647db54e07847235030d9575c.

--- src/main/java/com/cloudcipher/cloudcipher_client/SettingController.java
+++ src/main/java/com/cloudcipher/cloudcipher_client/SettingController.java
@@ -3,20 +3,15 @@ package com.cloudcipher.cloudcipher_client;
 import com.cloudcipher.cloudcipher_client.utility.FileUtility;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Path;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class SettingController implements Initializable {
@@ -60,20 +55,5 @@ public class SettingController implements Initializable {
 
     public void handleConfigButtonClick() {
         FileUtility.openDirectory(System.getProperty("user.home") + "/cloudcipher/");
-    }
-
-    public void handleDebugButtonClick() {
-        Stage stage = new Stage();
-        FXMLLoader fxmlLoader = new FXMLLoader(CloudCipherClient.class.getResource("debug-view.fxml"));
-        try {
-            Scene scene = new Scene(fxmlLoader.load());
-            stage.setResizable(false);
-            stage.getIcons().add(new Image(Objects.requireNonNull(CloudCipherClient.class.getResourceAsStream("logo.png"))));
-            stage.setTitle("Debug");
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
